318d784b9fbfd8c1e0a12fcc6455202e260d40b4
Ishtrb <ishiitorben@gmail.com>
2021-09-01 21:54:47 -0300

    :lipstick: Change baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://webhook.site/29748371-5b65-4743-bcf4-877e72dab4a5'
+    baseURL: 'https://gamabackend-with-node.herokuapp.com/'
 })
